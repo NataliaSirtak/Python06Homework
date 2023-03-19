778944e68a428fe15eed740ac11910b36f7f42b6
Natalia <nsirtak@gmail.com>
2023-03-18 23:29:15 -0500

    3.3

--- lesson_03.py
+++ lesson_03.py
@@ -9,9 +9,11 @@ print(my_list[2][2])
 list_1 = ['Hi', 'ananas', 2, None, 75, 'pizza', 36, 100]
 #Using lambda and filter
 print(sum(filter(lambda x: isinstance(x, int), list_1)))
-
 # 3.3. Превратите лист ['cat', 'dog', 'horse', 'cow'] в кортеж
-
+animal = ['cat', 'dog', 'horse', 'cow']
+animal = tuple(animal)
+print(animal)
+print(type(animal))
 # 3.4. Напишите программу, которая определяет, какая семья больше.
 #       1) Программа имеет два input() - например, family_1, family_2.
 #       2) Членов семьи нужно перечислить через запятую.
